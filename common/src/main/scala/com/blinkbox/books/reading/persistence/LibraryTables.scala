4fac47bccddadf3bebe4b50fb17c9e020effb049
Mohammed Abdulrazeg <mohammed@blinkbox.com>
2014-11-24 10:48:51 +0000

    Using `filter` instead of `withFilter`

--- common/src/main/scala/com/blinkbox/books/reading/persistence/LibraryTables.scala
+++ common/src/main/scala/com/blinkbox/books/reading/persistence/LibraryTables.scala
@@ -112,7 +112,7 @@ trait LibraryTables[Profile <: JdbcProfile] extends TablesContainer[Profile] {
     libraryItems.filter(b => b.isbn === isbn && b.userId === userId)
 
   private def getUserLibrary(count: ConstColumn[Long], offset: ConstColumn[Long], userId: Column[Int]):  lifted.Query[LibraryItems, LibraryItem, Seq] =
-    libraryItems.withFilter(b => b.userId === userId).drop(offset).take(count)
+    libraryItems.filter(b => b.userId === userId).drop(offset).take(count)
 
   private def getLibraryItemMedia(isbn: Column[String]): lifted.Query[LibraryMedia, LibraryItemLink, Seq] =
     libraryMedia.filter(_.isbn === isbn)
