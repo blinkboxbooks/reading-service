2fb0d94bc916c1cee871f5384c036545ac0dd487
Tori Jo Lau <toril@blinkbox.com>
2014-12-08 17:27:22 +0000

    Removed method not used in this version of automation

--- features/step_definitions/public/get_library_steps.rb
+++ features/step_definitions/public/get_library_steps.rb
@@ -2,11 +2,6 @@ When(/^I request my library$/) do
   get_library
 end
 
-When(/I request a library item( which does not exist in my library)?$/) do |missing|
-  isbn = missing ? 9780297859406 : 9780007197545
-  get_library_item(isbn)
-end
-
 Then(/^the response is a list (?:that is empty|containing (#{CAPTURE_INTEGER}) library items)$/) do |count|
   count ||= 0
   expect(@response_data['items'].size).to eq(count)
